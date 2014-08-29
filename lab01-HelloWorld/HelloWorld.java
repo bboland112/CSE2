e0ca6c8713cfee563d4fd3b8c400780896f22155
bboland112 <bpb215@lehigh.edu>
2014-08-29 13:47:49 +0000

    updated HelloWorld.java

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,16 @@
+//Brian Boland
+//Lab 01-Hello World
+
+public class HelloWorld{  //define the class
+    public static void main(String[] args) { //start the method
+    System.out.println("########################");  //The following lines of code will print out the contents of the parethesi 
+    System.out.println("########################");
+    System.out.println("#                      #");
+    System.out.println("#  Hello World 2014!!  #");
+    System.out.println("#                      #");
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("test");
+}
+    
+}
