dc5fa928e7229c5b72e1f0bdcfd2e2b12f546dae
bboland112 <bpb215@lehigh.edu>
2014-09-01 21:29:36 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,21 @@
+//hw 01
+//Welcome Class
+//Brian Boland
+
+public class WelcomeClass { //define the class
+    public static void main(String[] args) {
+        System.out.println("  -----------"); //The following lines of code will print out the contents of the quotations
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-b--p--b--2--1--5-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v  ");
+        System.out.println("This summer I traveled out to Yellowstone National Park ");
+        System.out.println("where I encountered an hour traffic jam due to bison roaming on the road");
+        
+    }
+    
+    
+}
